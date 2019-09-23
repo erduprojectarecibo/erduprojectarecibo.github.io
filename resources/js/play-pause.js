8ec73cb886602c0b29309d94ca2a41146bab8f5f
Andy G Vélez <velez.andy58@gmail.com>
2019-09-23 15:30:29 -0400

    Table img updating

--- resources/js/play-pause.js
+++ resources/js/play-pause.js
@@ -2,8 +2,6 @@
 let play = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 4l12 6-12 6z"/></svg>`;
 let pause = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z"/></svg>`;
 
-$('#play-range').value = 0;
-
 setTimeout(() => {
     $('#play-pause-button').html(play);
     document.getElementById('play-pause-button').addEventListener('click', function () {
